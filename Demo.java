bc75eaee44aee5b56672f73e46c9e6a3ad346d37
erenyeager71 <varalu71@gmail.com>
2026-08-25 12:29:33 +0530

    Added feature branch changes

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to DevOps");git status
+        System.out.println("Welcome to DevOps");
+        System.out.println("This is feature branch");
     }
 }
